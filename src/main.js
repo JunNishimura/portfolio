d64120f9d2148bc209b84e6ae1e253fba08dacda
Jun Nishimura <n.junjun0303@gmail.com>
2021-05-19 00:16:02 +0900

    Add FontAwesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import './assets/sass/style.scss'
 
-createApp(App).use(store).use(router).mount('#app')
+// fontawesome settings
+library.add(faFacebookSquare)
+
+createApp(App)
+.use(store)
+.use(router)
+.component("font-awesome-icon", FontAwesomeIcon)
+.mount('#app')
