1c88f3acc57b2174d681ed02820e9d02fef96c43
Jun Nishimura <n.junjun0303@gmail.com>
2021-05-20 18:33:12 +0900

    Add favicon

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,21 @@
 module.exports = {
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            title: 'JunNishimura',
+        }
+    },
     publicPath: process.env.NODE_ENV = 'production'
         ? '/portfolio/'
-        : '/'
+        : '/',
+    // css: {
+    //     loaderOptions: {
+    //         scss: {
+    //             prependData: `
+    //                 @import "@/assets/sass/_variables.scss";
+    //                 @import "@/assets/sass/style.scss";
+    //             `
+    //         }
+    //     }
+    // }
 }
